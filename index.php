c6afb2b710414827025b791dcb51869de2f3c484
DamianoSpampinato <ghgg585@gmail.com>
2024-04-30 16:54:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$franco = 'franco';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="contatore.php" method="GET">
+        <textarea name="paragraph" id="" required cols="30" rows="10"></textarea>
+        <input name="censured" type="text">
+        <button type="submit">Invialo</button>
+    </form>
+</body>
+</html>
